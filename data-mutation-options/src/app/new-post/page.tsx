13b14200ba86eff9deb7cd1b44c10c3b31bd07bf
luecha-work <luecha.kanm@gmail.com>
2024-05-22 15:46:53 +0700

    Feedback with useFormStatus hook

--- data-mutation-options/src/app/new-post/page.tsx
+++ data-mutation-options/src/app/new-post/page.tsx
@@ -1,4 +1,6 @@
+import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
   async function createPost(formData: any) {
@@ -7,12 +9,18 @@ export default function NewPostPage() {
     const image = formData.get("image");
     const content = formData.get("content");
 
+    console.log("====================================");
+    console.log(`image: ${image.name}`);
+    console.log("====================================");
+
     storePost({
       imageUrl: "",
       title,
       content,
       userId: 1,
     });
+
+    redirect("/feed");
   }
 
   return (
@@ -37,8 +45,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows={5} />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
