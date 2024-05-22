d6366d1eafdf1a23cc3bb37add3f980913e387f6
luecha-work <luecha.kanm@gmail.com>
2024-05-22 18:31:09 +0700

    Uploading & Storing Image To Cloudinary

---
+++ data-mutation-options/src/actions/posts.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { uploadImage } from "@/lib/cloudinary";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
+export async function createPost(
+  prevState: { errors: string[] },
+  formData: FormData
+): Promise<{ errors: string[] }> {
+  const title = formData.get("title") as string;
+  const image = formData.get("image") as File;
+  const content = formData.get("content") as string;
+
+  let errors: string[] = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required");
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required");
+  }
+
+  if (!image || !(image instanceof File) || image.size === 0) {
+    errors.push("Image is required");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  let imageUrl;
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      "Image upload failed, post was not created. Please try again later."
+    );
+  }
+
+  await storePost({
+    imageUrl,
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/feed");
+}
--- data-mutation-options/src/app/new-post/page.tsx
+++ data-mutation-options/src/app/new-post/page.tsx
@@ -1,45 +1,6 @@
+import { createPost } from "@/actions/posts";
 import PostForm from "@/components/post-form";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(
-    prevState: { errors: string[] },
-    formData: FormData
-  ): Promise<{ errors: string[] }> {
-    "use server";
-    const title = formData.get("title") as string;
-    const image = formData.get("image") as File;
-    const content = formData.get("content") as string;
-
-    let errors: string[] = [];
-
-    if (!title || title.trim().length === 0) {
-      errors.push("Title is required");
-    }
-
-    if (!content || content.trim().length === 0) {
-      errors.push("Content is required");
-    }
-
-    if (!image || !(image instanceof File) || image.size === 0) {
-      errors.push("Image is required");
-    }
-
-    if (errors.length > 0) {
-      return { errors };
-    }
-
-    storePost({
-      imageUrl: "", // Process and store image as needed
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-    return { errors: [] };
-  }
-
   return <PostForm action={createPost} />;
 }
