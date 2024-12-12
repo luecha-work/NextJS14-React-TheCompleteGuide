4f9baf4303ea07d21d46a8666340f1f73d84c213
luecha.kan <luecha.kanm@gmail.com>
2024-12-12 23:26:47 +0700

    A Look Behind The Scenes

--- page-pre-rendering-and-data-fetching/src/pages/index.tsx
+++ page-pre-rendering-and-data-fetching/src/pages/index.tsx
@@ -1,43 +1,46 @@
+import { Product } from "@/models/product";
+import fs from "fs/promises";
+import path from "path";
 import React from "react";
-
-interface Product {
-  id: number;
-  name: string;
-}
 
 interface HomePageProps {
   products: Product[];
 }
 
 const HomePage: React.FC<HomePageProps> = ({ products }) => {
+  if (!Array.isArray(products)) {
+    return <div>No products available</div>;
+  }
+
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.name}</li>
+        <li key={product.id}>{product.title}</li>
       ))}
     </ul>
   );
 };
 
-export async function getServerSideProps() {
-  return {
-    props: {
-      products: [
-        {
-          id: 1,
-          name: "Product 1",
-        },
-        {
-          id: 2,
-          name: "Product 2",
-        },
-        {
-          id: 3,
-          name: "Product 3",
-        },
-      ],
-    },
-  };
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "src", "data/dummy-backend.json");
+
+  try {
+    const jsonData = await fs.readFile(filePath, "utf-8");
+    const parsedData = JSON.parse(jsonData);
+
+    return {
+      props: {
+        products: parsedData.products,
+      },
+    };
+  } catch (error) {
+    console.error("Error reading data file:", error);
+    return {
+      props: {
+        products: [],
+      },
+    };
+  }
 }
 
 export default HomePage;
