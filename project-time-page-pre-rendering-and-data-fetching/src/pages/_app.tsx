dc6a3af77e78a602142284d8365739cfe41334c8
luecha.kan <luecha.kanm@gmail.com>
2024-12-15 17:11:02 +0700

    Working with the "_app.js" File (and Why)

--- project-time-page-pre-rendering-and-data-fetching/src/pages/_app.tsx
+++ project-time-page-pre-rendering-and-data-fetching/src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import Layout from "@/components/layout/layout";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        {/* <title>Next Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
