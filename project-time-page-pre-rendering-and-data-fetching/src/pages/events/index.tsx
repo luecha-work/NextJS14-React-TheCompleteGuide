1a2f4f30a4430cc7f839b83aea8401fe19cf3258
luecha.kan <luecha.kanm@gmail.com>
2024-12-15 15:11:38 +0700

    Working on the "All Events" Page

--- project-time-page-pre-rendering-and-data-fetching/src/pages/events/index.tsx
+++ project-time-page-pre-rendering-and-data-fetching/src/pages/events/index.tsx
@@ -1,13 +1,18 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
 
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
-import { getAllEvents } from "../../../dummy-data";
+import { getAllEvents } from "@/helpers/auth-utail";
+import { Event } from "@/models/event";
+import { useRouter } from "next/router";
 
-function AllEventsPage() {
+interface AllEventsPageProps {
+  events: Event[];
+}
+
+function AllEventsPage(props: AllEventsPageProps) {
   const router = useRouter();
-  const events = getAllEvents();
+  const events = props.events;
 
   function findEventsHandler(year: string, month: string) {
     const fullPath = `/events/${year}/${month}`;
@@ -21,6 +26,17 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
