ba70a9a6f5dbb596c93009bee46b4f9906118078
luecha.kan <luecha.kanm@gmail.com>
2024-12-21 16:29:05 +0700

    Adding A Post Grid & Post Items

--- build-full-blog-atoz/src/components/posts/post-item.tsx
+++ build-full-blog-atoz/src/components/posts/post-item.tsx
@@ -1,5 +1,45 @@
-function PostItem() {
-  return <div>Enter</div>;
+import Image from "next/image";
+import Link from "next/link";
+import classes from "./post-item.module.css";
+
+interface Post {
+  title: string;
+  image: string;
+  excerpt: string;
+  date: string;
+  slug: string;
+}
+
+interface PostItemProps {
+  post: Post;
+}
+
+function PostItem({ post }: PostItemProps) {
+  const { title, image, excerpt, date, slug } = post;
+
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link href={`/posts/${slug}`}>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
 }
 
 export default PostItem;
--- build-full-blog-atoz/src/components/posts/posts-grid.tsx
+++ build-full-blog-atoz/src/components/posts/posts-grid.tsx
@@ -1,14 +1,23 @@
 import PostsItem from "./post-item";
-
 import classes from "./posts-grid.module.css";
 
-function PostsGrid(props) {
-  const { posts } = props;
+interface Post {
+  title: string;
+  image: string;
+  excerpt: string;
+  date: string;
+  slug: string;
+}
 
+interface PostsGridProps {
+  posts: Post[];
+}
+
+function PostsGrid({ posts }: PostsGridProps) {
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostsItem />
+        <PostsItem key={post.slug} post={post} />
       ))}
     </ul>
   );
