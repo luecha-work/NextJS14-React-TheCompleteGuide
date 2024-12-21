f8b027373d7c8eb82631f22d0be0cf8dc849e2fc
luecha.kan <luecha.kanm@gmail.com>
2024-12-21 15:51:20 +0700

    Adding The "Hero" Component

--- build-full-blog-atoz/src/components/home-page/hero.tsx
+++ build-full-blog-atoz/src/components/home-page/hero.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
 function Hero() {
-  return <div>Enter</div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.jfif"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+        ></Image>
+      </div>
+      <h1>Hi, I&apos;m Max</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like React
+      </p>
+    </section>
+  );
 }
 
 export default Hero;
