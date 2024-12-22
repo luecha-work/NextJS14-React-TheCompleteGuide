d856745e3a687f2b07acfaecbede9910f47203d5
luecha.kan <luecha.kanm@gmail.com>
2024-12-22 19:04:13 +0700

    Adding Server-Side Page Guards (And When To Use Which Approach)

--- nextjs-page-authentication/src/pages/profile.tsx
+++ nextjs-page-authentication/src/pages/profile.tsx
@@ -1,7 +1,26 @@
 import UserProfile from "@/components/profile/user-profile";
+import { GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/react";
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
 
 export default ProfilePage;
