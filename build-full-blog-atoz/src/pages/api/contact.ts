65115efc575de29e302419a2d349c0d518172dab
luecha.kan <luecha.kanm@gmail.com>
2024-12-22 12:08:19 +0700

    Adding The Contact API Route

---
+++ build-full-blog-atoz/src/pages/api/contact.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+    // Store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    console.log(newMessage);
+    res.status(201).json({ message: "Successfully stored message!" });
+  }
+}
