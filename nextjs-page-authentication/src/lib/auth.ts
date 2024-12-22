5a840bdc188e6bbe6a398ec19e6c4dae4ed9d395
luecha.kan <luecha.kanm@gmail.com>
2024-12-22 17:40:59 +0700

    Adding the "Credentials Auth Provider" & User Login Logic

--- nextjs-page-authentication/src/lib/auth.ts
+++ nextjs-page-authentication/src/lib/auth.ts
@@ -1,4 +1,9 @@
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
+
+export async function verifyPassword(password: string, hashedPassword: string) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+}
 
 export async function hashPassword(password: string): Promise<string> {
   return await hash(password, 12);
---
+++ nextjs-page-authentication/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import { verifyPassword } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials) {
+          throw new Error("No credentials provided");
+        }
+
+        const { email, password } = credentials;
+
+        const client = await connectToDatabase();
+
+        try {
+          const usersCollection = client.db("local").collection("users");
+
+          const user = await usersCollection.findOne({ email });
+
+          if (!user) {
+            client.close();
+            throw new Error("No user found!");
+          }
+
+          const isValid = await verifyPassword(password, user.hashedPassword);
+
+          if (!isValid) {
+            client.close();
+            throw new Error("Invalid password!");
+          }
+
+          client.close();
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          console.log(`Error authorizing user: ${error}`);
+
+          client.close();
+          throw new Error("Authentication failed!");
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 24 * 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
