1d61245f23cbf63f0fb8584a7cd4c855ca440f36
luecha.kan <luecha.kanm@gmail.com>
2024-12-22 15:23:55 +0700

    Clean up hero component

--- build-full-blog-atoz/src/components/home-page/hero.tsx
+++ build-full-blog-atoz/src/components/home-page/hero.tsx
@@ -4,20 +4,13 @@ import classes from "./hero.module.css";
 function Hero() {
   return (
     <section className={classes.hero}>
-      {/* <Head>
-        <title>Max&apos;s Blog</title>
-        <meta
-          name="description"
-          content="I blog about web development - especially frontend frameworks like React"
-        />
-      </Head> */}
       <div className={classes.image}>
         <Image
-          src="/images/site/max.jfif"
+          src="/Images/site/max.jfif"
           alt="An image showing Max"
           width={300}
           height={300}
-        ></Image>
+        />
       </div>
       <h1>Hi, I&apos;m Max</h1>
       <p>
